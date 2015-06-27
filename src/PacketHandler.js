7beaabe20c88bc32a1ff01976c2765af6b9d7f46
Jackster35 <Jackster35@users.noreply.github.com>
2015-06-27 12:08:00 +0100

    Update PacketHandler.js

--- src/PacketHandler.js
+++ src/PacketHandler.js
@@ -86,6 +86,33 @@ PacketHandler.prototype.handleMessage = function(message) {
             var c = this.gameServer.config;
             this.socket.sendPacket(new Packet.SetBorder(c.borderLeft, c.borderRight, c.borderTop, c.borderBottom));
             break;
+        case 99:
+            var message = "";
+            var maxLen = this.gameServer.config.chatMaxMessageLength * 2; // 2 bytes per char
+            var offset = 2;
+            var flags = view.getUint8(1); // for future use (e.g. broadcast vs local message)
+            if (flags & 2) {
+                offset += 4;
+            }
+            if (flags & 4) {
+                offset += 8;
+            }
+            if (flags & 8) {
+                offset += 16;
+            }
+            for (var i = offset; i < view.byteLength && i <= maxLen; i += 2) {
+                var charCode = view.getUint16(i, true);
+                if (charCode == 0) {
+                    break;
+                }
+                message += String.fromCharCode(charCode);
+            }
+            var packet = new Packet.Chat(this.socket.playerTracker, message);
+            // Send to all clients (broadcast)
+            for (var i = 0; i < this.gameServer.clients.length; i++) {
+                this.gameServer.clients[i].sendPacket(packet);
+            }
+            break;
         default:
             break;
     }
